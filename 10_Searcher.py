815644a3efa4d4bb88995b9bf0826998edeb3600
Udit <uditknishad2119@gmail.com>
2021-01-12 22:55:54 +0530

    Nothing much just cleaning the code

--- 10_Searcher.py
+++ 10_Searcher.py
@@ -16,74 +16,73 @@ NamesDictinary = {}
 
 
 def MakeText():
-    """
-     This method will make the text at the random index with the names from the nameslist
-     Then this will intialise the names and the index in the list in the dictinary
-    """
-  # * This is importing  random module for randomly generated numbers for names
-    import random
+  """
+    This method will make the text at the random index with the names from the nameslist
+    Then this will intialise the names and the index in the list in the dictinary
+  """
+# * This is importing  random module for randomly generated numbers for names
+  import random
 
-  # * This is the list of name which will have an index
-    nameslist = ['Udit', 'Yachna', 'Aryan', 'Karan', 'Deepak']
+# * This is the list of name which will have an index
+  nameslist = ['Udit', 'Yachna', 'Aryan', 'Karan', 'Deepak']
 
-  # ! This is the main dict in which will intialise the index then we will search for them
-    NamesDictinary = {}
+# ! This is the main dict in which will intialise the index then we will search for them
+  NamesDictinary = {}
 
-  # * In this loop we are intialising random index
-    for name in nameslist:
-        index = random.randint(1, len(nameslist))
-        NamesDictinary.update({name: index})
-  # @ Printing that index for surity
-    print(NamesDictinary)
+# * In this loop we are intialising random index
+  for name in nameslist:
+      index = random.randint(1, len(nameslist))
+      NamesDictinary.update({name: index})
+# @ Printing that index for surity
+  print(NamesDictinary)
 
 
 def LoadingAnimation(textToBeShowen, numberofdots=5):
-    """
-          This function will animate the dots after the text is showen 
-          This will be like the downloading animation but in the terminal
-    """
-    import time
-    import sys
+  """
+        This function will animate the dots after the text is showen 
+        This will be like the downloading animation but in the terminal
+  """
+  import time
+  import sys
 
-    for n_dots in range(numberofdots):
-        sys.stdout.write(f'\r{textToBeShowen}{"." * n_dots}')
-        time.sleep(0.8)
+  for n_dots in range(numberofdots):
+      sys.stdout.write(f'\r{textToBeShowen}{"." * n_dots}')
+      time.sleep(0.8)
 
-    print('')
+  print('')
 
 
 def Search():
-    """
-     This method is the main coroutine that will initialise the indexes
-      and also Search for the names
-    """
+  """
+    This method is the main coroutine that will initialise the indexes
+    and also Search for the names
+  """
 
-    # ! This is the initialisation of the text
-    LoadingAnimation("Initialising", 8)
-    MakeText()
+  # ! This is the initialisation of the text
+  LoadingAnimation("Initialising", 8)
+  MakeText()
 
-    # ? This is the searching coroutine
-    print('Initialised!')
-    while True:
-        name = (yield)
+  # ? This is the searching coroutine
+  print('Initialised!')
+  while True:
+      name = (yield)
 
-        if NamesDictinary.get(name):
-            print(f'Name is present and index is {NamesDictinary.get(name)}')
-        else:
-            print('Either name is not present or wrong input!')
-        print('--------------------------------------------------------------------------------')
+      if NamesDictinary.get(name):
+          print(f'Name is present and index is {NamesDictinary.get(name)}')
+      else:
+          print('Either name is not present or wrong input!')
+      print('--------------------------------------------------------------------------------')
 
 
 # ? Execution
 if __name__ == '__main__':
+  NameBook = Search()
 
-    NameBook = Search()
+  next(NameBook)
 
-    next(NameBook)
-
-    while True:
-        name = input('Which name do you want to search for: \n').capitalize()
-        if name == '1':
-            break
-        else:
-            NameBook.send(name)
+  while True:
+      name = input('Which name do you want to search for: \n').capitalize()
+      if name == '1':
+          break
+      else:
+          NameBook.send(name)
