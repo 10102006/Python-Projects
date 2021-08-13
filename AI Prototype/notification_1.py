686aadd88eb5bf1c1af8ce49c9197a79c8cd46dc
Udit <uditknishad2119@gmail.com>
2021-08-13 15:21:31 +0530

    notification prototype done

--- AI Prototype/notification_1.py
+++ AI Prototype/notification_1.py
@@ -8,7 +8,17 @@ This will notify the user when a function is run and also it will have some time
 # @ Imports
 
 # * Defining
+from plyer import notification
+# import time
+
+# time.sleep(30)
 
 # ? Implementation
 if __name__ == "__main__":
-   print("Hello world")
+  notification.notify(
+    title='testing',
+    message='message',
+    app_icon=None,
+    timeout=10,
+  )
+  print("Hello world")
