0a227867a206654100b9a1d79cbbc07f78676140
Udit <uditknishad2119@gmail.com>
2021-08-13 15:14:20 +0530

    Set up AI

---
+++ AI Prototype/App.py
@@ -0,0 +1,12 @@
+"""
+  Overview>
+
+"""
+
+# @ Imports
+
+# * Defining
+
+# ? Implementation
+if __name__ == "__main__":
+   print("Hello world")
---
+++ AI Prototype/notification_1.py
@@ -0,0 +1,14 @@
+"""
+  Overview>
+
+This will notify the user when a function is run and also it will have some timer and stuff
+
+"""
+
+# @ Imports
+
+# * Defining
+
+# ? Implementation
+if __name__ == "__main__":
+   print("Hello world")
